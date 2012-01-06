3a369cf19f64d4152486e00c8507e6bc49b637b0
reth <reth@intruded.net>
2012-01-07 01:20:58 +1000

    * parse_query to parse PQL from the web server * added ability to write to arbitary sockets to print_board

--- pixeld/board.c
+++ pixeld/board.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define COLOUR_SIZE 	6
 #define COST_SIZE 	3
 #define OID_SIZE	4
 
-struct {
+#define ROWS		1000
+#define COLS		1200
+
+struct pixel {
 	char colour[COLOUR_SIZE];
 	char cost[COST_SIZE];
 	char oid[OID_SIZE];
-} pixel;
+};
 
 struct pixel 
 **init_board(unsigned int rows, unsigned int cols)
 {
 	unsigned int i, j;
 	struct pixel **board;
-	board = malloc(rows * sizeof(pixel *));
+	board = malloc(rows * sizeof(struct pixel *));
 	
 	for (i = 0; i < rows; i++) {
-		board[i] = malloc(sizeof(pixel) * cols);
+		board[i] = malloc(sizeof(struct pixel) * cols);
 		for (j = 0; j < cols; j++)
 			board[i][j].colour[0] = '.';
 	}
@@ -30,7 +34,7 @@ struct pixel
 int
 write_pixel(struct pixel **b, struct pixel *p, int r, int c)
 {
-	struct pixel *pp = b[r][c];
+	struct pixel *pp = &b[r][c];
 
 	if (p == NULL) {
 		p->colour[0] = '.';
@@ -48,18 +52,48 @@ write_pixel(struct pixel **b, struct pixel *p, int r, int c)
 }
 
 void 
-print_board(pixel **b, unsigned int rows, unsigned int cols) {
-	unsigned int i, j;
+print_board(int s, struct pixel **b) {
+	FILE *f;
 	struct pixel *p;
+	unsigned int i, j;
 
-	for (i = 0; i < rows; i++) {
-		for (j = 0; j < cols; j++) {
+	if ((f = fdopen(s, "w+")) == NULL)
+		return;
+
+	for (i = 0; i < ROWS; i++) {
+		for (j = 0; j < COLS; j++) {
 			p = &b[i][j];
 			if (p->colour[0] == '.')
-				printf(". ");
+				fprintf(f, ".");
 			else
-				printf("%s%s%s ", p->colour, p->cost, p->oid);
+				fprintf(f, "%s%s%s", 
+				        p->colour, 
+					p->cost, 
+					p->oid);
 		}
-		printf("\n");
+		fprintf(f, "\n");
 	}
+}
+
+/* parse PQL command from the web server */
+int
+parse_query(int sock, char *qry, struct pixel **board)
+{
+	char *s = malloc(strlen(qry)); 
+	char *qp;
+
+	if (qry[0] == 'g') {
+		print_board(sock, board);
+		return 1;
+	}
+
+	strcpy(s, qry);
+	if (strtok(s, " ") == NULL)
+		return 0;
+	
+	qp = malloc(strlen(qry));
+	memcpy(qp, qry + strlen(s) + 1, strlen(qry) - strlen(s));
+
+	free(s);
+	return 1;
 }
