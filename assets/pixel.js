e4c1c9d33348720eb3bd70135bf0e9fc1d53889c
Saul <saul@pixenomics.com>
2012-02-04 00:07:45 +0000

    * tooltip ops

--- assets/pixel.js
+++ assets/pixel.js
@@ -340,8 +340,8 @@ $(function() {
 			$("#tooltip .price").text("$" + (pixel.cost / 100).toFixed(2));
 			
 			$("#tooltip").show().css({
-				left: globalPos.left + 15,
-				top: globalPos.top
+				left: globalPos.left + 130,
+				top: globalPos.top + 105
 			});
 			
 		}).click(function(e) {
